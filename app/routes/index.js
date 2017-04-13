019064ac2de34e410d28e6fe4bf21de5f626d579
Claire Gui <lesadamaro@inaby.com>
2017-04-13 20:32:50 +0000

    add new files

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,7 +18,6 @@ router.get('/', function(req, res, next) {
     });
 });
 
-/* GET home page. */
 router.get('/bottles', function(req, res, next) {
 
     connection.query('SELECT * FROM bottles', function(error, results, fields) {
@@ -30,6 +29,19 @@ router.get('/bottles', function(req, res, next) {
             'message': 'here is a message',
             'bottles': results
         });
+    });
+});
+
+router.get('/bottles/new', function(req, res, next) {
+
+    var sql = "INSERT INTO bottles VALUES ( null, 'welcome to bottles story', '2017-04-03 00:00:00', 0)";
+
+    connection.query(sql, function(error, results, fields) {
+        if (error) throw error;
+
+        console.log(results);
+
+        res.send('New bottle added');
     });
 });
 
