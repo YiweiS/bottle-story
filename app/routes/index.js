db2b85e64af2e321303306d744ea65a4e260ff8b
Claire Gui <lesadamaro@inaby.com>
2017-04-13 20:14:09 +0000

    add database file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'clairegui',
+    password: '',
+    database: 'bottlestory'
+});
+
+connection.connect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+/* GET home page. */
+router.get('/bottles', function(req, res, next) {
+
+    connection.query('SELECT * FROM bottles', function(error, results, fields) {
+        if (error) throw error;
+
+        console.log(results);
+
+        res.render('bottles', {
+            'message': 'here is a message',
+            'bottles': results
+        });
+    });
 });
 
 module.exports = router;
